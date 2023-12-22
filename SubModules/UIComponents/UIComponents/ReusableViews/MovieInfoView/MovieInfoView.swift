ce990812c701d7680222d03bb2b4882b93fc84f9
Eslam Abo Elfetouh <eslamaboelfetouh@gmail.com>
2023-12-22 16:09:02 +0200

    Create and config MovieInfoView, and MovieInfoViewProtocol.

---
+++ SubModules/UIComponents/UIComponents/ReusableViews/MovieInfoView/MovieInfoView.swift
@@ -0,0 +1,35 @@
+//
+//  MovieInfoView.swift
+//  UIComponents
+//
+//  Created by Eslam Abo El Fetouh on 22/12/2023.
+//
+
+import UIKit
+
+public final class MovieInfoView: UIView {
+   
+    @IBOutlet private weak var movieImageView: UIImageView!
+    @IBOutlet private weak var movieTitleLabel: UILabel!
+    @IBOutlet private weak var movieSubitleLabel: UILabel!
+    @IBOutlet private weak var movieRatingLabel: UILabel!
+
+    // MARK: - Initializers
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        instantiateNib()
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        instantiateNib()
+    }
+
+    public func configView(with item: MovieInfoViewItemProtocol) {
+        self.movieImageView.loadImage(with: item.imagePath)
+        self.movieTitleLabel.text = item.title
+        self.movieSubitleLabel.text = item.subTitle
+        self.movieRatingLabel.text = item.averageRating
+        
+    }
+}
---
+++ SubModules/UIComponents/UIComponents/ReusableViews/MovieInfoView/MovieInfoViewItemProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  MovieInfoViewItemProtocol.swift
+//  UIComponents
+//
+//  Created by Eslam Abo El Fetouh on 22/12/2023.
+//
+
+import Foundation
+
+public protocol MovieInfoViewItemProtocol: MovieCellItemProtocol {
+    var averageRating: String { get }
+}
