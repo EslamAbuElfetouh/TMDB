2c680335d26229d5659c061df37467eb9219ca37
Eslam Abo Elfetouh <eslamaboelfetouh@gmail.com>
2023-12-22 20:26:37 +0200

    Create the MovieListPresenterTests class Factory method and create the SUT method.

--- TMDBTests/UnitTssts/MovieList/MovieListPresenterTests.swift
+++ TMDBTests/UnitTssts/MovieList/MovieListPresenterTests.swift
@@ -10,4 +10,33 @@ import XCTest
 
 final class MovieListPresenterTests: XCTestCase {
 
+    // MARK: Factory Methods
+    func movieListPresenterFactory(view: MovieListControllerProtocol,
+                                   interactor: MovieListPresenterInteractorProtocol,
+                                   router: MovieListRouterProtocol) -> MovieListPresenter {
+        MovieListPresenter(view: view, interactor: interactor, router: router)
+    }
+
+    func mockMovieListViewFactory() -> MovieListControllerProtocol {
+        MockMovieListView()
+    }
+
+    func mockMovieListInteractorFactory() -> MovieListPresenterInteractorProtocol {
+        MockMovieListInteractor()
+    }
+
+    func mockMovieListRouterFactory() -> MovieListRouterProtocol {
+        MockMovieListRouter()
+    }
+    
+    /// Creates the system under test (SUT) for MovieListPresenter.
+    /// - Returns: MovieListPresenter instance with mock dependencies for testing.
+    func createSystemUnderTest() -> MovieListPresenter {
+        movieListPresenterFactory(view: mockMovieListViewFactory(),
+                                  interactor: mockMovieListInteractorFactory(),
+                                  router: mockMovieListRouterFactory())
+    }
+    
+    // TODO: Testcases
+    
 }
