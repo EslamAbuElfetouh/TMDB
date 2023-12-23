a6b42d0eab98af53ae6017be9c46aad2840aca2d
Eslam Abo Elfetouh <eslamaboelfetouh@gmail.com>
2023-12-24 01:26:11 +0200

    Update ImageBaseUrl value

--- TMDB/Utilities/Constants.swift
+++ TMDB/Utilities/Constants.swift
@@ -10,5 +10,5 @@ import Foundation
 struct Constants {
     static let token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhM2M3NDQyMjIyMTQ3ZmRjYTk2NDExZjM3YWVmZTQ0NSIsInN1YiI6IjU3YjBkOWQ3OTI1MTQxMzE4MzAwMDBkMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vNyyH28o3wltb4D6AO0HHyVAbgClb1RkpapLq_ghmrg"
     static let baseUrl = "https://api.themoviedb.org/"
-    static let baseImageURL = baseUrl + "t/p/w500/"
+    static let baseImageURL = "https://image.tmdb.org/t/p/w500/"
 }
