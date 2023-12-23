0fdbd94138da3dd748a8452ff84100d904f141b1
Eslam Abo Elfetouh <eslamaboelfetouh@gmail.com>
2023-12-24 01:24:08 +0200

    Update MovieCell config method parameter item optional

--- SubModules/UIComponents/UIComponents/UICollectionViewCells/MovieCell/MovieCell.swift
+++ SubModules/UIComponents/UIComponents/UICollectionViewCells/MovieCell/MovieCell.swift
@@ -13,7 +13,8 @@ public final class MovieCell: UICollectionViewCell {
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var subtitleLabel: UILabel!
     // MARK: Configurations
-    public func configCell(with item: MovieCellItemProtocol) {
+    public func configCell(with item: MovieCellItemProtocol?) {
+        guard let item else { return }
         movieImageView.loadImage(with: item.imagePath)
         titleLabel.text = item.title
         subtitleLabel.text = item.subtitle
