29bf21fb60b5751cce8afe92ff094124011f81fe
Eslam Abo Elfetouh <eslamaboelfetouh@gmail.com>
2023-12-24 13:57:54 +0200

    Add Numeric Extension to use Numerics as String

---
+++ TMDB/Extensions/Numeric+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Numeric+Extension.swift
+//  TMDB
+//
+//  Created by Eslam Abo El Fetouh on 24/12/2023.
+//
+
+import Foundation
+
+extension Numeric {
+    var stringValue: String {
+        return String(describing: self)
+    }
+}
